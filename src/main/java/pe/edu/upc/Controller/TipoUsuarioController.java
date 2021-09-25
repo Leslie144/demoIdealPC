4b8300bdc96b9697bf3a129bd69416076de313f6
leslie2952 <85373184+Leslie144@users.noreply.github.com>
2021-09-25 11:44:43 -0500

    tipo usuario

--- src/main/java/pe/edu/upc/Controller/TipoUsuarioController.java
+++ src/main/java/pe/edu/upc/Controller/TipoUsuarioController.java
@@ -32,11 +32,12 @@ public class TipoUsuarioController {
 	public void init() { // #5
 		tipousuarioCenter = new TipoUsuarioEntities();
 		listaTipoUsuarios = new ArrayList<TipoUsuarioEntities>();
+		this.list();
 	}
 
 	public String newTipoUsuario() { // #7
 		this.setTipousuarioCenter(new TipoUsuarioEntities());
-		return "TipoUsuario.xhtml";
+		return "tipoUsuario.xhtml";
 	}
 
 	public void insert() { // #8
@@ -88,7 +89,5 @@ public class TipoUsuarioController {
 	public void setListaTipoUsuarios(List<TipoUsuarioEntities> listaTipoUsuarios) {
 		this.listaTipoUsuarios = listaTipoUsuarios;
 	}
-
-	
 
 }
